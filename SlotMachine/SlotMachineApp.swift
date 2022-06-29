d4565833575edb3af5e568a01e79d428ee9304fc
Vanya Sharma <vanya.sharma0815@gmail.com>
2022-06-29 14:00:29 +0530

    SlotMachine SwiftUI

---
+++ SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Divyansh Dwivedi on 30/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
